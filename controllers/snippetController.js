63380e4f618f83d45359200274983036cf231e9e
Angel <aleangrodriguez123@gmail.com>
2018-05-30 10:59:01 -0500

    Added try/catch

--- controllers/snippetController.js
+++ controllers/snippetController.js
@@ -2,18 +2,27 @@ const Snippet = require("../models/Snippet");
 
 module.exports = {
     async create(req, res) {
-        const { name, github } = req.body
-        const authorId = ""
-        const snippet = await Snippet.create({
-            name,
-            github,
-            authorId
-        })
-        return res.json(snippet)
+        try {
+            const { name, github } = req.body
+            const authorId = ""
+            const snippet = await Snippet.create({
+                name,
+                github,
+                authorId
+            })
+            return res.json(snippet)
+        } catch (error) {
+            return res.status(500).send(error)
+        }
+        
     },
     async list(req, res) {
-        const { name } = req.query
-        const snippets = await Snippet.find({ name: new RegExp(name, "i") })
-        return res.json(snippets)
+        try {
+            const { name } = req.query
+            const snippets = await Snippet.find({ name: new RegExp(name, "i") })
+            return res.json(snippets)
+        } catch (error) {
+            return res.status(500).send(error)            
+        }
     }
 }
