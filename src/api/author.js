1a466657457e495656e0edbf5fda04a58bf122e0
Angel <aleangrodriguez123@gmail.com>
2018-07-07 07:33:58 -0500

    refactor

--- src/api/author.js
+++ src/api/author.js
@@ -4,7 +4,7 @@ import VueResource from "vue-resource";
 Vue.use(VueResource)
 
 const HOST = process.env.ROOT_HOST || "http://localhost:3000/api";
-debugger
+
 export default {
     create(payload) {
         return Vue.http.post(`${HOST}/author`, payload);
