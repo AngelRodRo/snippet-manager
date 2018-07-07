d64847878a2d1f1e3308b9c4203b6bab76c06388
Angel <aleangrodriguez123@gmail.com>
2018-07-07 07:33:02 -0500

    New config

--- webpack.config.js
+++ webpack.config.js
@@ -106,7 +106,7 @@ if (process.env.NODE_ENV === 'production') {
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
-      'process.env': process.env
+      'process.env.ROOT_HOST': process.env.ROOT_HOST
     }),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
