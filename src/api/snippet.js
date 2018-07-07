d83c9bd02dd17ff292c523fb17ea9ad4abce0614
Angel <aleangrodriguez123@gmail.com>
2018-07-07 07:54:23 -0500

    hotfix routes

--- src/api/snippet.js
+++ src/api/snippet.js
@@ -3,13 +3,13 @@ import VueResource from "vue-resource";
 
 Vue.use(VueResource)
 
-const HOST =  process.env.ROOT_HOST || "http://localhost:3000";
+const HOST =  process.env.ROOT_HOST || "http://localhost:3000/api";
 
 export default {
     list(name) {
-        return Vue.http.get(`${HOST}/api/snippet?name=${name}`).then(response => response.data);
+        return Vue.http.get(`${HOST}/snippet?name=${name}`).then(response => response.data);
     },
     create(credentials) {
-        return Vue.http.post(`${HOST}/api/snippet`, credentials).then(response => response.data);
+        return Vue.http.post(`${HOST}/snippet`, credentials).then(response => response.data);
     }
 }
